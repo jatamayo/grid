8e97361df8ea7f803e274167a79b9306cf08c6a4
jatamayo <jalejandrotamayo@hotmail.com>
2019-02-16 13:34:57 -0600

    style part 2 ended

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,3 @@
-
 var app = angular.module("myApp", []);
 app.controller("myCtrl", function($scope) {
   $scope.options = [
@@ -8,3 +7,4 @@ app.controller("myCtrl", function($scope) {
     {"Image": "Images/image3.jpg", "Title": "Title No.3", "Subtitle": "Subtitle No.3", "Description": "Un apartamento, también llamado departamento o piso, es una unidad de vivienda que comprende una o más habitaciones diseñadas para proporcionar instalaciones completas para un individuo o una pequeña familia.", "Price": 800,"Size": 100,"Room": 3,"Bathroom": 2,"Parking": 3}
   ]
 });
+
