1f91a9360687389a94c59cef6a631b023fa8953c
jatamayo <jalejandrotamayo@hotmail.com>
2019-02-16 22:23:17 -0600

    style stage no3

--- JS/main.js
+++ JS/main.js
@@ -7,4 +7,3 @@ app.controller("myCtrl", function($scope) {
     {"Image": "Images/image3.jpg", "Title": "Title No.3", "Subtitle": "Subtitle No.3", "Description": "Un apartamento, también llamado departamento o piso, es una unidad de vivienda que comprende una o más habitaciones diseñadas para proporcionar instalaciones completas para un individuo o una pequeña familia.", "Price": 800,"Size": 100,"Room": 3,"Bathroom": 2,"Parking": 3}
   ]
 });
-
